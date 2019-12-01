8ea57937942265b0cf1add294b0cabf7a5364054
guilh <guilh@guilherme.local>
2019-12-01 20:53:48 -0300

    arquivo sql com as tabelas incluído

---
+++ web/WEB-INF/db_script/Lava_Rapido.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE cliente (
+ CPF varchar(11) NOT NULL PRIMARY KEY,
+    Nome varchar(100)
+);
+
+CREATE TABLE veiculo (
+Placa_veiculo varchar(7) NOT NULL PRIMARY KEY,
+Tipo_veiculo varchar(5) NOT NULL,
+Marca_veiculo varchar(2),
+Modelo_veiculo varchar(20)
+);
+
+CREATE TABLE servico (
+Tipo_servico varchar(50) NOT NULL PRIMARY KEY,
+Valor int
+);
