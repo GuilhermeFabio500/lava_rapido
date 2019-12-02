83a638d91d1a92d827bb954211128b0195ecc827
guilh <guilh@guilherme.local>
2019-12-01 21:23:36 -0300

    classe java cliente.java, funções criadas

--- src/java/br/com/fatecpg/lava_rapido/Cliente.java
+++ src/java/br/com/fatecpg/lava_rapido/Cliente.java
@@ -5,6 +5,12 @@
  */
 package br.com.fatecpg.lava_rapido;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author guilh
@@ -12,6 +18,32 @@ package br.com.fatecpg.lava_rapido;
 public class Cliente {
     String Nome;
     String CPF;
+    
+    public static ArrayList<Cliente> getList() throws Exception{
+        ArrayList<Cliente> list = new ArrayList();
+        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        String url = "jdbc:derby://localhost:1527/sample";
+        String user = "app";
+        String password = "app";                
+        Connection con = DriverManager.getConnection(url, user, password);
+        Statement stmt = con.createStatement();
+        String sql = "SELECT NOME, CPF, FROM APP.CLIENTE";
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next()){
+            Cliente c = new Cliente(
+                    rs.getString("NOME"),
+                    rs.getString("CPF")
+            );
+            list.add(c);
+        }
+        rs.close();stmt.close();con.close();
+        return list;
+    }
+
+    public Cliente(String Nome, String CPF) {
+        this.Nome = Nome;
+        this.CPF = CPF;
+    }
 
     public String getNome() {
         return Nome;
@@ -28,6 +60,5 @@ public class Cliente {
     public void setCPF(String CPF) {
         this.CPF = CPF;
     }
-    
     
 }
